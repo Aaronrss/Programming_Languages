24f46ee9197218acb9113f4fcb2a13c315e0c803
Aaron Rosas <46721112+Aaronrss@users.noreply.github.com>
2022-05-09 10:41:04 -0500

    Delete rmDuplicates.pl

--- Prolog/rmDuplicates.pl
+++
@@ -1,6 +0,0 @@
-remove_duplicates([], []).
-remove_duplicates([Head | Tail], Result) :-
-    member(Head, Tail), !,
-    remove_duplicates(Tail, Result).
-remove_duplicates([Head | Tail], [Head | Result]) :-
-    remove_duplicates(Tail, Result).
